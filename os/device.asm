76ad834f66583f9f38ea8b026554c95c5fabf665
Eric Wimberley <blank89@gmail.com>
2020-03-13 00:33:43 -0400

    Fixing bug in copy procedure.

--- os/device.asm
+++ os/device.asm
@@ -1,22 +1,31 @@
 define ZP_BUFFER $7F
 define ZP_BUFFER_LEN $80 
 
-define CP_TO_ZP_POINTER $00
-define CONSOLE_PRINT_POINTER $01
-define CONSOLE_READ_POINTER $02
+define CP_TO_ZP_FROM_POINTER $00
+define CP_TO_ZP_TO_POINTER $01
+define CONSOLE_PRINT_POINTER $02
+define CONSOLE_READ_POINTER $03
 
 .org $1200
 
 ;x = from
 ;y = to 
 cp_static_to_zp:
-STX CP_TO_ZP_POINTER 
-CPY CP_TO_ZP_POINTER 
+STX CP_TO_ZP_FROM_POINTER 
+LDX #$00
+STX CP_TO_ZP_TO_POINTER
+cp_static_to_zp_loop:
+LDX CP_TO_ZP_FROM_POINTER
+CPY CP_TO_ZP_FROM_POINTER ;if from == to
 BEQ cp_static_to_zp_done 
 LDA STATIC,X
-STA ZP_BUFFER,X 
 INX
-JMP cp_static_to_zp 
+STX CP_TO_ZP_FROM_POINTER 
+LDX CP_TO_ZP_TO_POINTER
+STA ZP_BUFFER,X
+INX
+STX CP_TO_ZP_TO_POINTER
+JMP cp_static_to_zp_loop 
 cp_static_to_zp_done:
 RTS
 
@@ -29,7 +38,7 @@ define CONSOLE_BUFF $0213
 ;y = to 
 console_print:
 STX CONSOLE_PRINT_POINTER
-CPY CONSOLE_PRINT_POINTER
+CPY CONSOLE_PRINT_POINTER ;if from == to
 BEQ console_print_done
 LDA ZP_BUFFER,X
 STA CONSOLE_BUFF
