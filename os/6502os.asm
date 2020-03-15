8dec79a9feda4f2f507f0419c90484f38fdd069a
Eric Wimberley <blank89@gmail.com>
2020-03-14 21:50:56 -0400

    Disk IO procedures.

--- os/6502os.asm
+++ os/6502os.asm
@@ -7,23 +7,26 @@ LDY #$08
 JSR console_print
 
 start:
+;print console sentinal
 LDX #$10
 LDY #$12 
 JSR cp_static_to_zp
 LDX #$00
 LDY #$02 
 JSR console_print
+;read 10 bytes max
 LDX #$00
 LDY #$10
 JSR console_read
 LDX #$00
 LDY #$10
 JSR console_print
+;print nl
 LDX #$15
 LDY #$16 
 JSR cp_static_to_zp
 LDX #$00
 LDY #$01
 JSR console_print
-
+BRK
 JMP start ;OS loop
--- os/device.asm
+++ os/device.asm
@@ -5,9 +5,11 @@ define CP_TO_ZP_FROM_POINTER $00
 define CP_TO_ZP_TO_POINTER $01
 define CONSOLE_PRINT_POINTER $02
 define CONSOLE_READ_POINTER $03
+define CONSOLE_READ_LEN_TMP $04
 
 .org $1200
 
+;Copy static page to ZP buffer
 ;x = from
 ;y = to 
 cp_static_to_zp:
@@ -15,12 +17,14 @@ STX CP_TO_ZP_FROM_POINTER
 LDX #$00
 STX CP_TO_ZP_TO_POINTER
 cp_static_to_zp_loop:
-LDX CP_TO_ZP_FROM_POINTER
 CPY CP_TO_ZP_FROM_POINTER ;if from == to
 BEQ cp_static_to_zp_done 
+;load byte from static
+LDX CP_TO_ZP_FROM_POINTER
 LDA STATIC,X
 INX
 STX CP_TO_ZP_FROM_POINTER 
+;store byte to zp
 LDX CP_TO_ZP_TO_POINTER
 STA ZP_BUFFER,X
 INX
@@ -34,6 +38,7 @@ define CONSOLE_S $0211 ;status 0=empty, 1=buffered
 define CONSOLE_C $0212 ;status 0=not reading, 1=reading
 define CONSOLE_BUFF $0213
 
+;Print ZP buffer to console
 ;x = from
 ;y = to 
 console_print:
@@ -47,16 +52,19 @@ JMP console_print
 console_print_done:
 RTS
 
+;Read console to ZP buffer
 ;x = to
 ;y = max_len
 ;TODO check max_len
 console_read:
 LDA #$01
 STA CONSOLE_C
+length_poll_loop:
 LDA CONSOLE_S
 CMP #$00
-BEQ console_read ;wait for input
+BEQ length_poll_loop ;wait for input
 LDY CONSOLE_LEN
+STY CONSOLE_READ_LEN_TMP
 console_read_loop:
 CPY #$00 
 BEQ console_read_working_done 
@@ -68,10 +76,39 @@ JMP console_read_loop
 console_read_working_done:
 LDA #$00
 STA CONSOLE_C
+LDY CONSOLE_READ_LEN_TMP ;return string length
 RTS
 
-define DISK_C $0220 ;control
-define DISK_S $0221 ;status 0=ready, 1=busy
-define DISK_BUFF $0222
+define DISK_POS $0220 
+define DISK_LEN $0221
+define DISK_S $0222 ;status 0=ready, 1=write, 2=read, 3=busy, 4=failed
 
+;Write ZP buffer to disk (uses direct memory access)
+;x = disk position
+;y = length
+disk_write:
+STX DISK_POS
+STY DISK_LEN
+LDA #$01
+STA DISK_S
+disk_write_wait_loop:
+LDA DISK_S
+CMP #$00
+BNE disk_write_wait_loop
+;TODO support failed status
+RTS
 
+;Read disk to ZP buffer (uses direct memory access)
+;x = disk position
+;y = length
+disk_read:
+STX DISK_POS
+STY DISK_LEN
+LDA #$02
+STA DISK_S
+disk_write_wait_loop:
+LDA DISK_S
+CMP #$00
+BNE disk_write_wait_loop
+;TODO support failed status
+RTS
